6d115867e3c1dfc481ffb3e16127ca6edb05d75b
tofu4956 <wprmoqwof49smg762tdseryaaiwe@gmail.com>
2014-10-14 19:41:07 +0900

    Revert "sqlite"
    This reverts commit 0bb6108742230f0f28e775b67c05b16825dca8f7.

--- Celestial Craft Fleet Fleet Battle Formation Simurator/src/com/tofu4956/ccffbfs/FBFSSeilorinfo.java
+++ Celestial Craft Fleet Fleet Battle Formation Simurator/src/com/tofu4956/ccffbfs/FBFSSeilorinfo.java
@@ -1,5 +1,4 @@
-//コードの汚さは一人前
-//DB再生成
+
 package com.tofu4956.ccffbfs;
 
 import java.sql.Connection;
@@ -18,17 +17,17 @@ public static void main(String[] args)  {
 
 	try {
 		Class.forName("org.sqlite.JDBC");
-		conn = DriverManager.getConnection("jdbc:sqlite:../Celestial Craft Fleet Fleet Battle Formation Simurator/bin/com/tofu4956/ccffbfs/sqlfile/Seilordatabase.s3db", "tofuccfbfs", "youneedeattofu");
+		conn = DriverManager.getConnection("jdbc:sqlite:../Celestial Craft Fleet Fleet Battle Formation Simurator/bin/com/tofu4956/ccffbfs/sqlfile/Seilordatabase", "tofuccfbfs", "youneedeattofu");
 		stmt = conn.createStatement();
 
 		//テーブル作成
-		stmt.executeUpdate("create table seilordata( name string, rank integer, Dhp INTEGER, Datk INTEGER, Mskill TEXT, FskillR TEXT, SkillS TEXT)" );
+		stmt.executeUpdate("create table test1( name string, rank integer )" );
 
 		//値を入力する
-		stmt.execute( "insert into seilordata values ( 'サキ', 5, 764, 692,'ブリザードロア', 'フリーズロア', 'アイシクルレイン')" );
+		stmt.execute( "insert into test1 values ( 'サキ', 5 )" );
 
 		//結果を表示する
-		rs = stmt.executeQuery("select * from seilordata");
+		rs = stmt.executeQuery("select * from test1");
 		while(rs.next()) {
 			System.out.println(rs.getString("name"));
 			System.out.println(rs.getInt("rank"));
